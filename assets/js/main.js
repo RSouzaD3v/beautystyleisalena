0c3ec91ec8f162dd3979c729110862c1bf59e3a9
Th3K1ngDev <rafaelf.souzan02@gmail.com>
2023-11-12 05:43:51 -0300

    Primeiro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
-var iconMenu = document.querySelector('.iconMenu i');
-var menuMobile = document.querySelector('.navMenuMobile');
-var main = document.querySelector('main');
+function menuHidden(){
+    var iconMenu = document.querySelector('.iconMenu i');
+    var menuMobile = document.querySelector('.navMenuMobile');
+    var main = document.querySelector('main');
 
 iconMenu.addEventListener('click', (e) => {
     if (menuMobile.classList.contains('active')){
@@ -9,3 +10,21 @@ iconMenu.addEventListener('click', (e) => {
         menuMobile.classList.add('active');
     }
 })
+}
+
+function showButtonBt(){
+    var backTop = document.querySelector('#backTop')
+    var pos = window.scrollY;
+    console.log(pos)
+    if(pos > 600){
+        backTop.style.display = 'block';
+    }else{
+        backTop.style.display = 'none';
+    }
+
+}
+
+setInterval(showButtonBt, 500)
+
+
+window.onload = menuHidden();
